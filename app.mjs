b28a73634d27ff41902c9d405b5225e77d8edcd9
Carter Cobb <carter.cobb72@gmail.com>
2022-07-07 11:41:04 -0600

    first commit

---
+++ app.mjs
@@ -0,0 +1,26 @@
+import express from "express";
+import * as nsr from "node-server-router";
+import os from "os";
+import cluster from "cluster";
+import { NODE_ENV, PORT as ENV_PORT } from "./keys.mjs";
+
+const PORT = ENV_PORT || 7777;
+
+if (cluster.isPrimary && NODE_ENV === "production") {
+  for (var _ = 0; _ < os.cpus().length; _++) cluster.fork();
+  cluster.on("exit", (worker) => {
+    console.log(`[${worker.process.pid}] Died; spinning new instance up...`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
+
+  nsr.RouteFactory.applyRoutesTo(app, {
+    api_version: "/v1",
+    log_configured: true,
+  });
+
+  app.listen(PORT, () =>
+    console.log(`[${process.pid}] Started on port: ${PORT}`)
+  );
+}
---
+++ keys.mjs
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+// Nodejs generals
+export const NODE_ENV = process.env.NODE_ENV;
+export const PORT = process.env.PORT;
