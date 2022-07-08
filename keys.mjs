b4ab7c34da45644bab1dd860d8d2a0825b5d5dd8
Carter Cobb <carter.cobb72@gmail.com>
2022-07-07 18:13:20 -0600

    added database, JWT, & Session conf keys

--- keys.mjs
+++ keys.mjs
@@ -4,3 +4,17 @@ dotenv.config();
 // Nodejs generals
 export const NODE_ENV = process.env.NODE_ENV;
 export const PORT = process.env.PORT;
+
+// SQL Conf
+export const SQL_CONFIG_SERVER = process.env.SQL_CONFIG_SERVER;
+export const SQL_CONFIG_USER = process.env.SQL_CONFIG_USER;
+export const SQL_CONFIG_PASSWORD = process.env.SQL_CONFIG_PASSWORD;
+export const SQL_CONFIG_DATABASE = process.env.SQL_CONFIG_DATABASE;
+export const SQL_CONFIG_INSTANCE = process.env.SQL_CONFIG_INSTANCE;
+export const SQL_CONFIG_PORT = process.env.SQL_CONFIG_PORT;
+
+// JWT secret
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+// Session secret
+export const SESSION_SECRET = process.env.SESSION_SECRET;
