db75a1dae7d8e8c98f47a8150151471ec0ce4fa4
Carter Cobb <carter.cobb72@gmail.com>
2022-07-07 18:13:02 -0600

    added comments & logging

--- app.mjs
+++ app.mjs
@@ -1,27 +1,37 @@
 import express from "express";
 import * as nsr from "node-server-router";
+import SessionConfig from "./session/sessionConf.mjs";
 import os from "os";
 import cluster from "cluster";
 import { NODE_ENV, PORT as ENV_PORT } from "./keys.mjs";
+import logger from "./logger.mjs";
 
 const PORT = ENV_PORT || 7777;
 
+logger.log(
+  `\r\n\x1b[33m---------------------- \x1b[37m${NODE_ENV.toUpperCase()} ENVIRONMENT \x1b[33m----------------------\r\n`
+);
+
+// Master Process
 if (cluster.isPrimary && NODE_ENV === "production") {
   for (var _ = 0; _ < os.cpus().length; _++) cluster.fork();
-  cluster.on("exit", (worker) => {
-    console.log(`[${worker.process.pid}] Died; spinning new instance up...`);
+  cluster.on("exit", () => {
+    logger.error("Died; spinning new instance up...");
     cluster.fork();
   });
 } else {
+  // Child Processs
   const app = express();
   app.use(express.json());
 
+  // Inject routes
   nsr.RouteFactory.applyRoutesTo(app, {
     api_version: "/v1",
-    log_configured: true,
   });
+  logger.success("Routes configured");
 
-  app.listen(PORT, () =>
-    console.log(`[${process.pid}] Started on port: ${PORT}`)
-  );
+  // Configure session
+  SessionConfig.configure(app);
+
+  app.listen(PORT, () => logger.success(`Started on port: ${PORT}`));
 }
